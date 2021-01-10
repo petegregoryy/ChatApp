92c3c3416d47aa56a574e6bf53e29037c44fca24
Pete G <boris.the.alien@gmail.com>
2021-01-10 16:03:31 +0000

    added client input

--- Client.py
+++ Client.py
@@ -1,12 +1,15 @@
 import socket
 import threading
 
-_HOST = input("What server would you like to connect to: ")
+# _HOST = input("What server would you like to connect to: ")
+_HOST = "127.0.0.1"
 _PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((_HOST,_PORT))
-    s.sendall(b'Hello World')
-    data = s.recv(1024)
+    while True:
+        usr = input("> ")
+        s.send(usr.encode('utf-8'))
+        data = s.recv(1024)
 
-print('Recieved' , repr(data))
+        print('Recieved' , repr(data))
