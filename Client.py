cd1320af8c0a98586c97f3696faee6f4e1a38bc5
Pete G <boris.the.alien@gmail.com>
2021-01-10 15:56:39 +0000

    added basic echo server

--- Client.py
+++ Client.py
@@ -1,1 +1,12 @@
-# This is the Client file
+import socket
+import threading
+
+_HOST = input("What server would you like to connect to: ")
+_PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((_HOST,_PORT))
+    s.sendall(b'Hello World')
+    data = s.recv(1024)
+
+print('Recieved' , repr(data))
--- Server.py
+++ Server.py
@@ -1,1 +1,17 @@
-# This is a the server file
+import socket
+import threading
+
+_HOST = '127.0.0.1'
+_PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((_HOST,_PORT))
+    s.listen()
+    conn,addr = s.accept()
+    with conn:
+        print('Connection from ', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
