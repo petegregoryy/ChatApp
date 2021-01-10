af7f32b97e45b2e48d794760fa8b7ccdcd842165
Pete G <boris.the.alien@gmail.com>
2021-01-10 16:36:48 +0000

    added thread functions (unused)

--- Client.py
+++ Client.py
@@ -6,10 +6,14 @@ _HOST = "127.0.0.1"
 _PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((_HOST,_PORT))
+    _sock = s.connect((_HOST,_PORT))
     while True:
         usr = input("> ")
         s.send(usr.encode('utf-8'))
         data = s.recv(1024)
 
         print('Recieved' , repr(data))
+
+# def recieveThread(sock):
+
+# def sendThread(sock):
