665a789c9ed798f142f3ee5e2973379ac2b39348
Pete G <boris.the.alien@gmail.com>
2021-01-10 18:16:34 +0000

    Commented

--- Server.py
+++ Server.py
@@ -8,51 +8,52 @@ global users
 users = [""]
 
 def process_message(data, conn):
-    string = data.decode("utf-8")
-    command = string[0:4]
-    msg = string[4:]
-    print ("CMD: " + command)
+    string = data.decode("utf-8") # Decode reply
+    command = string[0:4] # Get first 4 letters for command
+    msg = string[4:] # Get rest of string
+    # Print them
+    print ("CMD: " + command) 
     print("MSG: " + msg)
 
 
-    if command == "USER":
-        unique = True
-        for user in users:
+    if command == "USER": # Check if the command was a USER command
+        unique = True # Create Unique
+        for user in users: # For each user in the users list check if it matches the message, if it does, the name isnt unique
             if user == msg:
                 unique = False
                 break
             else:
                 unique = True
                 
-        if unique:
+        if unique: # If the name is unique, append the username to the users list, print that and send an OK response
             users.append(msg)
             print(f"Adding {msg} to users list")
             print(users)
             conn.sendall(b"250 OK")
             print ("Sending 250 OK")
-        else:
+        else: # Otherwise, send a 550 to show an error
             conn.sendall(b'550 User duplicate')
             print ("Sending 550")
-    elif command == "DATA":
+    elif command == "DATA": # If the command is DATA, send the message back to the user
         conn.sendall(str.encode(msg))
         print (f"Sending {msg}")
 
-def ProcessThread(s, sock, addr):
+def ProcessThread(s, sock, addr): # Function for controlling the connection with the socket
         with sock:
             print('Connection from ', addr)
             while True:
-                data = sock.recv(1024)
-                process_message(data,sock)
+                data = sock.recv(1024) # Receive data
+                process_message(data,sock) # send the data and the socket to be processed
 
-                if not data:
+                if not data: # Quit if it doesn't receive data
                     break
                 # sock.sendall(data)
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((_HOST,_PORT))
+        s.bind((_HOST,_PORT)) # Bind socket and start listening
         s.listen()
-        conn,address = s.accept()
-        process = threading.Thread(target=ProcessThread, args=(s,conn,address))
-        process.start()
+        conn,address = s.accept() # When connection occurs, set conn and adress to the information of the connection
+        process = threading.Thread(target=ProcessThread, args=(s,conn,address)) # create a thread using the conn and address information
+        process.start() # Run the thread
 
 
